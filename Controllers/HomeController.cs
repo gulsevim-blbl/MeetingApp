94ffd11aa124e26475877ade84d814147dd36ae7
gulsevim-blbl <gulsevimblbl@gmail.com>
2025-09-19 01:41:27 +0300

    ViewBag & ViewData

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace  MettingApp.Controllers 
+namespace MettingApp.Controllers
 {
-    public class HomeController : Controller {
-        public IActionResult Index() {
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            int saat = DateTime.Now.Hour;
+
+            // ViewBag.Selamlama = saat > 12 ? "Günaydın" : "İyi Günler";
+            // ViewBag.Username = "Gül Sevim";
+
+            ViewData["Selamlama"] = saat > 12 ? "Günaydın" : "İyi Günler";
+            ViewData["Username"] = "Gül Sevim";
+
+
             return View();
         }
 
